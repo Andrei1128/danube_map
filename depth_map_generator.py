0fa7cab78151f7068674e9014251fcf704c21980
Andrei-Ionuţ Nicolaev <andrei.nicolaev@365.univ-ovidius.ro>
2025-07-18 16:35:46 +0300

    add better config

--- depth_map_generator.py
+++ depth_map_generator.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from scipy.interpolate import griddata
-import plotly.express as px
 
 csv_file_path = r"C:\Users\Andrei\Desktop\danube_map\bathymetry_Danube.csv"
 
@@ -16,7 +15,7 @@ df = pd.read_csv(csv_file_path)
 df_filtered = df[
     (df['lat'] >= lat_min_range) & (df['lat'] <= lat_max_range) &
     (df['lon'] >= lon_min_range) & (df['lon'] <= lon_max_range) &
-    df['bathymetry'] > 0
+    (df['bathymetry'] > 0)
 ]
 
 df = df_filtered
@@ -26,8 +25,8 @@ lat_min, lat_max = lat_min_range, lat_max_range
 lon_min, lon_max = lon_min_range, lon_max_range
 
 # Create grid within your exact coordinate range
-lat_grid = np.linspace(lat_min, lat_max, 800)
-lon_grid = np.linspace(lon_min, lon_max, 800)
+lat_grid = np.linspace(lat_min, lat_max, 100)
+lon_grid = np.linspace(lon_min, lon_max, 100)
 lon_mesh, lat_mesh = np.meshgrid(lon_grid, lat_grid)
 
 # Interpolate bathymetry data
@@ -37,6 +36,15 @@ values = df['bathymetry'].values
 # Use griddata for interpolation
 bathymetry_grid = griddata(points, values, (lat_mesh, lon_mesh), method='linear')
 
+custom_colorscale = [
+    [0.0, "rgb(198, 237, 255)"],  # very light blue (shallow)
+    [0.2, "rgb(158, 202, 255)"],  # light-medium blue
+    [0.4, "rgb(108, 174, 214)"],  # medium blue
+    [0.6, "rgb(68, 135, 195)"],   # medium-dark blue
+    [0.8, "rgb(34, 89, 164)"],    # dark blue
+    [1.0, "rgb(10, 54, 130)"],    # deeper dark blue (not black)
+]
+
 # Create the contour map
 fig = go.Figure(data=
     go.Contour(
@@ -45,14 +53,25 @@ fig = go.Figure(data=
         y=lat_grid,
         showscale=False,
         line_smoothing=0.85,
+        colorscale = custom_colorscale,
+        line_width=1,
         contours=dict(
             showlabels = True,
+            start=0,
+            end=32,
+            size=1,
             labelfont = dict(
                 size = 12,
-                color = 'white',
+                color = 'white'
             )
         )
 ))
 
-# Save as HTML for easy viewing
+fig.update_layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+)
+
 fig.write_image("bathymetry_contour_map.svg",'svg')
